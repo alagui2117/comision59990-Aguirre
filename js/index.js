8fcbfb1013e4040dae3873b57a16042f3b6847d4
luis aguirre <lualagui@gmail.com>
2024-11-04 17:06:26 -0600

    terceraEntrega

--- js/index.js
+++ js/index.js
@@ -1,56 +1,131 @@
 //PARA REGISTRAR EN UN ALMACEN INTRODUCIR CONTRASEÑA PARA QUE PUEDAN INTRODUCIR PRODUCTOS O VER RESUMEN DE PRODUCTOS
+const carritoKey = "carrito"
 
 const PRODUCTOS = [
     {
+        id:1,
         nombre: "Azucar",
         precio: 30.50,
         cantidad: 4,
+        imagen:"https://plus.unsplash.com/premium_photo-1671130295824-37e73f098d46?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YXp1Y2FyfGVufDB8fDB8fHww"
+       
         
     },
     {
+        id:2,
         nombre: "Frijoles",
         precio: 20.70,
-        cantidad: 10
+        cantidad: 10,
+        imagen: "https://plus.unsplash.com/premium_photo-1671130295242-582789bd9861?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZnJpam9sZXN8ZW58MHx8MHx8fDA%3D"
     },
     {
+        id:3,
         nombre: "Arroz",
         precio: 22.50,
-        cantidad: 8
+        cantidad: 8,
+        imagen: "https://plus.unsplash.com/premium_photo-1705338026411-00639520a438?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXJyb3p8ZW58MHx8MHx8fDA%3D"
     },
 
 ];
+let CARRITO = []
 
 
 function main(){
-    if(seguridad()== false){
-        console.log("No tienes permiso para ingresar al sistema");
-        return;
-
-    }
-    let funcionalidad = prompt(
-        "ingresa la categoria \n" +
-        "1.-Productos\n"+
-        "2.-Pedidos");
-    switch(funcionalidad){
-        case "1":
-            productos()
-            break;
-        case "2":
-            pedidos()
-            break;
-                
-
-    
-    }
+  
+   verProductos()
+   verCarrito()
 }
 
 main()
 
 
+function addProducto(idProducto){
+    
+    let productoAgregado = CARRITO.filter(item => item.id == idProducto)
+
+    if (productoAgregado.length){
+        productoAgregado.map(item => {
+            item.cantidad+=1
+            item.total = Math.round(item.cantidad * item.precio)
+        })
+        localStorage.setItem(carritoKey, JSON.stringify(CARRITO))
+        verCarrito()
+        return
+    }
+
+
+    let producto = PRODUCTOS.find(item => item.id == idProducto)
+
+    if (producto){
+        let productoAgregar = {
+            ...producto,
+            cantidad:1,
+            total: producto.precio
+        }
+
+        CARRITO.push(productoAgregar)
+       
+    }
+    localStorage.setItem(carritoKey, JSON.stringify(CARRITO))
+    verCarrito()
+    
+}
+
+function removerProducto(idProducto){
+
+    CARRITO = CARRITO.filter(item => item.id != idProducto)
+
+    localStorage.setItem(carritoKey, JSON.stringify(CARRITO))
+    verCarrito()
+
+}
+
+function verCarrito(){
+
+    if (!CARRITO.length && localStorage.getItem(carritoKey)){
+        
+        let carritoStr  = localStorage.getItem(carritoKey)
+        CARRITO  = JSON.parse(carritoStr)
+
+
+    }
+   
+    
+    const listaDom = document.getElementById("lista-carrito")
+    let rows = "";
+    let total  = 0 
+    if (listaDom) {
+        CARRITO.forEach((producto, index) => {
+                total += producto.total
+                rows += `<div class="card w-100">
+                    <div class="card-body d-flex flex-wrap justify-content-end">
+                    <h5 class="card-title w-100"> <i class="bi bi-x-lg text-danger" onclick="removerProducto(${producto.id})"></i>  ${producto.nombre}</h5> <p></p>
+                    <p class="card-text price" >  ${producto.cantidad} unidades</p>
+              
+                    </div>
+                    <div class="card-footer">
+                              <p class="card-text price" >  $${producto.total} MXN </p>
+                    </div>
+                </div>`
+           
+             }
+        )
+        
+        rows += `<div class="card w-100">
+        <div class="card-body d-flex flex-wrap justify-content-end">
+        <h5 class="card-title w-100">  Total: $${total} MXN</h5>
+        </div>
+        </div>`
+        listaDom.innerHTML = rows
+    }
+    
+}
+
 
 function productos(){
 
-    let operacion = prompt("ingresa la operacion\n"+
+
+    /*let operacion = prompt("ingresa la operacion\n"+
          "1.- Ver productos\n"+
          "2.- Insertar producto\n"+
          "3.- Modificar producto"
@@ -68,15 +143,31 @@ function productos(){
             console.log("ingresaste a resumen mensual")
             modificarProducto();
             break;
-    }
+    }*/
 }
 
 function verProductos(){
     //alert(PRODUCTOS)
-    PRODUCTOS.forEach((producto, index) => {
-            console.log(`${index + 1}.-${producto.nombre}  ${producto.cantidad}  ${producto.precio}`) 
-        }
-    )
+    const listaDom = document.getElementById("lista-productos")
+    let cards = "";
+   
+    if (listaDom) {
+        PRODUCTOS.forEach((producto, index) => {
+            
+                cards += `<div class="card" style="width: 18rem;">
+                    <img src="${producto.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body d-flex flex-wrap justify-content-end">
+                    <h5 class="card-title w-100">${producto.nombre}</h5>
+                    <p class="card-text price" >$${producto.precio} MXN </p>
+                    <a href="#" class="btn btn-primary w-100" onclick="addProducto(${producto.id})"> Agregar</a>
+                    </div>
+                </div>`
+           
+             }
+        )
+        listaDom.innerHTML = cards
+    }
+    
     localStorage.setItem("productos", JSON.stringify(PRODUCTOS))
 }
 
@@ -144,7 +235,6 @@ function introducirProductos() {
             continuar = false;
         }
     }
-    console.log(PRODUCTOS)
     localStorage.setItem("productos", JSON.stringify(PRODUCTOS))
 
 }
